469fe9c8f26b1952ff5bf6b3c199990ecb573cc9
Devansh Khanna <devanshkhanna.9f.2020@gmail.com>
2025-09-19 11:27:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+var numberofDrums = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i<numberofDrums; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var buttonHTML = this.innerHTML;
+        makesound(buttonHTML);
+        buttonAnimation(buttonHTML);
+        switch (buttonHTML) {
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+        
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+                
+            case "j":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            
+            case "k":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;  
+                
+            case "l":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;    
+            default:
+                console.log(buttonHTML);
+        }
+    });
+}
+
+document.addEventListener("keypress", function(event){
+    makesound(event.key);
+    buttonAnimation(event.key)
+
+})
+
+function makesound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+    
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+            
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;  
+            
+        case "l":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;    
+        default:
+            console.log(buttonHTML);
+    }
+}
+
+function buttonAnimation(currentKey){
+    var animation = document.querySelector("." + currentKey);
+    animation.classList.add("pressed");
+    setTimeout(function(){
+        animation.classList.remove("pressed");
+    },100);
+}
+
+//Constructor Function
+/*function BellBoy (name, age, hasWorkPermit, langauges){
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.langauges = langauges;
+}
+
+var bellBoy1 = new BellBoy("Timmy", 19, true, ['French','German']);
+
+console.log(bellBoy1.name);*/
+
